551efcaf8a870de6356dce68cecd4ca69002e4ea
BenjyLagrainge <benjy.lagrainge@hotmail.com>
2026-09-04 16:11:17 +0200

    Import datetime module in moneywatcher.py

--- moneywatcher.py
+++ moneywatcher.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import datetime  
 from database import laad_data
 
 st.set_page_config(page_title="Moneywatcher", layout="wide")
